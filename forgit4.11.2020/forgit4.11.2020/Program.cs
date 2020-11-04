448c706826eb183c7f0cd6a64c91c1dc40d5b604
ilker171103 <ilker.2000@abv.bg>
2020-11-04 12:09:16 +0200

    init

---
+++ forgit4.11.2020/forgit4.11.2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace forgit4._11._2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to GIT/GITHUB!");
+        }
+    }
+}
